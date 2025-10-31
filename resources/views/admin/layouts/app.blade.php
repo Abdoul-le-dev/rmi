2c9a7da6f096631bedeafcd326f9776698e7f7b8
Stanislas <stanislasbayord79@gmail.com>
2025-10-31 09:31:38 +0100

    updates

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -19,9 +19,9 @@
     <link rel="stylesheet" href="/assets/admin/css/style.css">
     <link rel="stylesheet" href="/assets/admin/css/custom.css">
     <link rel="stylesheet" href="/assets/admin/css/components.css">
-    @if($isRtl)
+    {{-- @if($isRtl) --}}
         <link rel="stylesheet" href="/assets/admin/css/rtl.css">
-    @endif
+    {{-- @endif --}}
     <link rel="stylesheet" href="/assets/admin/vendor/daterangepicker/daterangepicker.min.css">
     <link rel="stylesheet" href="/assets/default/vendors/select2/select2.min.css">
 
