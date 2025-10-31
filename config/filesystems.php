254bccf7edd22f9c121dce3a2c492141095df4fd
Stanislas <stanislasbayord79@gmail.com>
2025-10-31 16:28:12 +0100

    updates

--- config/filesystems.php
+++ config/filesystems.php
@@ -123,7 +123,7 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => 'https://rmibucket053752723003.s3.us-east-1.amazonaws.com/rmiclass-prod-uploads/',
+            'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
