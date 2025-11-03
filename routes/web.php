3e34683caf8873a75f64baa9248dad4f958dda95
Stanislas <stanislasbayord79@gmail.com>
2025-11-03 12:15:00 +0100

    feat: custom lfm

--- routes/web.php
+++ routes/web.php
@@ -172,7 +172,7 @@ Route::group(['namespace' => 'Web', 'middleware' => ['check_mobile_app', 'impers
 
     Route::group(['middleware' => 'web.auth'], function () {
 
-        Route::group(['prefix' => 'laravel-filemanager','middleware' => 'lfm.s3'], function () {
+        Route::group(['prefix' => 'laravel-filemanager'], function () {
             \UniSharp\LaravelFilemanager\Lfm::routes();
         });
 
