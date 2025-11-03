a94fad380335ca9057a96e5a493fd4eb826a1325
Stanislas <stanislasbayord79@gmail.com>
2025-11-03 09:25:10 +0100

    updates

--- config/filesystems.php
+++ config/filesystems.php
@@ -127,7 +127,7 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'visibility' => 'public',
+            // 'visibility' => 'public',
             'root' => 'rmiclass-prod-uploads',
             'throw' => true,
         ],
