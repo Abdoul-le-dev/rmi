3b4475c3e11238fa82581d0903a48c297e8757ff
Stanislas <stanislasbayord79@gmail.com>
2025-11-04 17:08:33 +0100

    feat: img

--- resources/views/vendor/laravel-filemanager/index.blade.php
+++ resources/views/vendor/laravel-filemanager/index.blade.php
@@ -210,36 +210,36 @@
         label: lang['menu-rename'],
         multiple: false
       },
-      {
-        name: 'download',
-        icon: 'download',
-        label: lang['menu-download'],
-        multiple: true
-      },
+      // {
+      //   name: 'download',
+      //   icon: 'download',
+      //   label: lang['menu-download'],
+      //   multiple: true
+      // },
       // {
       //   name: 'preview',
       //   icon: 'image',
       //   label: lang['menu-view'],
       //   multiple: true
       // },
-      {
-        name: 'move',
-        icon: 'paste',
-        label: lang['menu-move'],
-        multiple: true
-      },
-      {
-        name: 'resize',
-        icon: 'arrows-alt',
-        label: lang['menu-resize'],
-        multiple: false
-      },
-      {
-        name: 'crop',
-        icon: 'crop',
-        label: lang['menu-crop'],
-        multiple: false
-      },
+      // {
+      //   name: 'move',
+      //   icon: 'paste',
+      //   label: lang['menu-move'],
+      //   multiple: true
+      // },
+      // {
+      //   name: 'resize',
+      //   icon: 'arrows-alt',
+      //   label: lang['menu-resize'],
+      //   multiple: false
+      // },
+      // {
+      //   name: 'crop',
+      //   icon: 'crop',
+      //   label: lang['menu-crop'],
+      //   multiple: false
+      // },
       {
         name: 'trash',
         icon: 'trash',
