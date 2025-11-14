71219308fdda9cf08e4ee57809b7d197fb22f3a7
Abdoul-le-dev <abdoulledev@gmail.com>
2025-11-14 14:35:21 +0100

    suite

--- resources/views/admin/teams/size_bare_add.blade.php
+++ resources/views/admin/teams/size_bare_add.blade.php
@@ -1,4 +1,4 @@
-<li class="flex flex-row ">
-    <img src="/teams/icon/apps-add.png" class="w-8 mr-4">
+<li class="flex">
+    <img src="/teams/icon/apps-add.png" class="w-4 mr-4">
     <a href="/suscriber">suscriber</a>
 </li>
