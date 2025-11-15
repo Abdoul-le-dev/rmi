1f24578f2053eebd55623459eb21daa9efa89c63
Abdoul-le-dev <abdoulledev@gmail.com>
2025-11-15 12:20:41 +0100

    suite

--- resources/views/admin/teams/size_bare_add.blade.php
+++ resources/views/admin/teams/size_bare_add.blade.php
@@ -1,6 +1,6 @@
 <li>
     
-    <a href="admin_d_fiacre/suscriber" class="nav-link">
+    <a href="/suscriber" class="nav-link">
         <img src="/teams/icon/apps-add.png" class="w-4 mr-4">
         <span class="">Suscriber</span>
     </a>
