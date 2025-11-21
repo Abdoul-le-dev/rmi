699908850942e7d171d98a7f3128d0ccbf8e8688
root <root@ip-10-0-0-12.ec2.internal>
2025-11-21 07:53:26 +0000

    f

--- storage/framework/views/803b5b69ca5d89535801457755b8263b4edbeb9f.php
+++
@@ -1,7 +0,0 @@
-<li>
-    
-    <a href="admin_d_fiacre/suscriber" class="nav-link">
-        <img src="/teams/icon/apps-add.png" class="w-4 mr-4">
-        <span class="">Suscriber</span>
-    </a>
-</li><?php /**PATH /var/www/rmi/resources/views/admin/teams/size_bare_add.blade.php ENDPATH**/ ?>
