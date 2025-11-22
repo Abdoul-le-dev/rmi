ef9c2744eb43477cf4b1bf02c569aad5ab421034
Stanislas <stanislasbayord79@gmail.com>
2025-11-22 10:56:55 +0100

    feat: debug

--- resources/views/web/default/forum/post_card.blade.php
+++ resources/views/web/default/forum/post_card.blade.php
@@ -103,6 +103,9 @@
                             ? updateImagePaths($post->description)
                             : updateImagePaths($topic->description) !!}
                     </div>
+                    @php
+                        dd($topic->description);
+                    @endphp
 
                     @if(!empty($post) and !empty($post->attach))
                         <div class="mt-auto d-inline-flex">
