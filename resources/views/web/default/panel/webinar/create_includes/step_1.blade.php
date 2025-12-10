20a71f993f3fab60392fd694eab5d0f2aaee2ece
Stanislas <stanislasbayord79@gmail.com>
2025-12-10 17:12:17 +0100

    feat: js quizz

--- resources/views/web/default/panel/webinar/create_includes/step_1.blade.php
+++ resources/views/web/default/panel/webinar/create_includes/step_1.blade.php
@@ -5,12 +5,15 @@
 <div class="row">
     <div class="col-12 col-md-4 mt-15">
 
-        @if(!empty(getGeneralSettings('content_translate')))
+        @if (!empty(getGeneralSettings('content_translate')))
             <div class="form-group">
                 <label class="input-label">{{ trans('auth.language') }}</label>
                 <select name="locale" class="custom-select {{ !empty($webinar) ? 'js-edit-content-locale' : '' }}">
-                    @foreach($userLanguages as $lang => $language)
-                        <option value="{{ $lang }}" @if(mb_strtolower(request()->get('locale', app()->getLocale())) == mb_strtolower($lang)) selected @endif>{{ $language }} {{ (!empty($definedLanguage) and is_array($definedLanguage) and in_array(mb_strtolower($lang), $definedLanguage)) ? '('. trans('public.content_defined') .')' : '' }}</option>
+                    @foreach ($userLanguages as $lang => $language)
+                        <option value="{{ $lang }}" @if (mb_strtolower(request()->get('locale', app()->getLocale())) == mb_strtolower($lang)) selected @endif>
+                            {{ $language }}
+                            {{ (!empty($definedLanguage) and is_array($definedLanguage) and in_array(mb_strtolower($lang), $definedLanguage)) ? '(' . trans('public.content_defined') . ')' : '' }}
+                        </option>
                     @endforeach
                 </select>
             </div>
@@ -23,34 +26,40 @@
             <label class="input-label d-block">{{ trans('panel.course_type') }}</label>
 
             <select name="type" class="custom-select @error('type')  is-invalid @enderror">
-                <option value="webinar" @if(!empty($webinar) and $webinar->isWebinar()) selected @endif>{{ trans('webinars.webinar') }}</option>
-                <option value="course" @if(!empty($webinar) and $webinar->type == 'course') selected @endif>{{ trans('webinars.video_course') }}</option>
-                <option value="text_lesson" @if(!empty($webinar) and $webinar->type == 'text_lesson') selected @endif>{{ trans('webinars.text_lesson') }}</option>
+                <option value="webinar" @if (!empty($webinar) and $webinar->isWebinar()) selected @endif>
+                    {{ trans('webinars.webinar') }}</option>
+                <option value="course" @if (!empty($webinar) and $webinar->type == 'course') selected @endif>
+                    {{ trans('webinars.video_course') }}</option>
+                <option value="text_lesson" @if (!empty($webinar) and $webinar->type == 'text_lesson') selected @endif>
+                    {{ trans('webinars.text_lesson') }}</option>
             </select>
 
             @error('type')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
             @enderror
         </div>
 
 
-        @if($isOrganization)
+        @if ($isOrganization)
             <div class="form-group mt-15 ">
                 <label class="input-label d-block">{{ trans('public.select_a_teacher') }}</label>
 
                 <select name="teacher_id" class="custom-select @error('teacher_id')  is-invalid @enderror">
-                    <option value="" {{ (!empty($webinar) and !empty($webinar->teacher_id)) ? '' : 'selected' }}>{{ trans('public.choose_instructor') }}</option>
-                    @foreach($teachers as $teacher)
-                        <option value="{{ $teacher->id }}" {{ (!empty($webinar) && $webinar->teacher_id == $teacher->id) ? 'selected' : '' }}>{{ $teacher->full_name }}</option>
+                    <option value="" {{ (!empty($webinar) and !empty($webinar->teacher_id)) ? '' : 'selected' }}>
+                        {{ trans('public.choose_instructor') }}</option>
+                    @foreach ($teachers as $teacher)
+                        <option value="{{ $teacher->id }}"
+                            {{ !empty($webinar) && $webinar->teacher_id == $teacher->id ? 'selected' : '' }}>
+                            {{ $teacher->full_name }}</option>
                     @endforeach
                 </select>
 
                 @error('teacher_id')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
         @endif
@@ -58,21 +67,26 @@
 
         <div class="form-group mt-15">
             <label class="input-label">{{ trans('public.title') }}</label>
-            <input type="text" name="title" value="{{ (!empty($webinar) and !empty($webinar->translate($locale))) ? $webinar->translate($locale)->title : old('title') }}" class="form-control @error('title')  is-invalid @enderror" placeholder=""/>
+            <input type="text" name="title"
+                value="{{ (!empty($webinar) and !empty($webinar->translate($locale))) ? $webinar->translate($locale)->title : old('title') }}"
+                class="form-control @error('title')  is-invalid @enderror" placeholder="" />
             @error('title')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
             @enderror
         </div>
 
         <div class="form-group mt-15">
             <label class="input-label">{{ trans('public.seo_description') }}</label>
-            <input type="text" name="seo_description" value="{{ (!empty($webinar) and !empty($webinar->translate($locale))) ? $webinar->translate($locale)->seo_description : old('seo_description') }}" class="form-control @error('seo_description')  is-invalid @enderror " placeholder="{{ trans('forms.50_160_characters_preferred') }}"/>
+            <input type="text" name="seo_description"
+                value="{{ (!empty($webinar) and !empty($webinar->translate($locale))) ? $webinar->translate($locale)->seo_description : old('seo_description') }}"
+                class="form-control @error('seo_description')  is-invalid @enderror "
+                placeholder="{{ trans('forms.50_160_characters_preferred') }}" />
             @error('seo_description')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
             @enderror
         </div>
 
@@ -80,15 +94,19 @@
             <label class="input-label">{{ trans('public.thumbnail_image') }}</label>
             <div class="input-group">
                 <div class="input-group-prepend">
-                    <button type="button" class="input-group-text panel-file-manager" data-input="thumbnail" data-preview="holder">
+                    <button type="button" class="input-group-text panel-file-manager" data-input="thumbnail"
+                        data-preview="holder">
                         <i data-feather="arrow-up" width="18" height="18" class="text-white"></i>
                     </button>
                 </div>
-                <input type="text" name="thumbnail" id="thumbnail" value="{{ !empty($webinar) ? $webinar->thumbnail : old('thumbnail') }}" class="form-control @error('thumbnail')  is-invalid @enderror" placeholder="{{ trans('forms.course_thumbnail_size') }}"/>
+                <input type="text" name="thumbnail" id="thumbnail"
+                    value="{{ !empty($webinar) ? $webinar->thumbnail : old('thumbnail') }}"
+                    class="form-control @error('thumbnail')  is-invalid @enderror"
+                    placeholder="{{ trans('forms.course_thumbnail_size') }}" />
                 @error('thumbnail')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
         </div>
@@ -97,15 +115,19 @@
             <label class="input-label">{{ trans('public.cover_image') }}</label>
             <div class="input-group">
                 <div class="input-group-prepend">
-                    <button type="button" class="input-group-text panel-file-manager" data-input="cover_image" data-preview="holder">
+                    <button type="button" class="input-group-text panel-file-manager" data-input="cover_image"
+                        data-preview="holder">
                         <i data-feather="arrow-up" width="18" height="18" class="text-white"></i>
                     </button>
                 </div>
-                <input type="text" name="image_cover" id="cover_image" value="{{ !empty($webinar) ? $webinar->image_cover : old('image_cover') }}" placeholder="{{ trans('forms.course_cover_size') }}" class="form-control @error('image_cover')  is-invalid @enderror"/>
+                <input type="text" name="image_cover" id="cover_image"
+                    value="{{ !empty($webinar) ? $webinar->image_cover : old('image_cover') }}"
+                    placeholder="{{ trans('forms.course_cover_size') }}"
+                    class="form-control @error('image_cover')  is-invalid @enderror" />
                 @error('image_cover')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
         </div>
@@ -115,11 +137,10 @@
 
             <div class="">
                 <label class="input-label font-12">{{ trans('public.source') }}</label>
-                <select name="video_demo_source"
-                        class="js-video-demo-source form-control"
-                >
-                    @foreach(\App\Models\Webinar::$videoDemoSource as $source)
-                        <option value="{{ $source }}" @if(!empty($webinar) and $webinar->video_demo_source == $source) selected @endif>{{ trans('update.file_source_'.$source) }}</option>
+                <select name="video_demo_source" class="js-video-demo-source form-control">
+                    @foreach (\App\Models\Webinar::$videoDemoSource as $source)
+                        <option value="{{ $source }}" @if (!empty($webinar) and $webinar->video_demo_source == $source) selected @endif>
+                            {{ trans('update.file_source_' . $source) }}</option>
                     @endforeach
                 </select>
             </div>
@@ -129,19 +150,24 @@
             <label class="input-label font-12">{{ trans('update.path') }}</label>
             <div class="input-group js-video-demo-path-input">
                 <div class="input-group-prepend">
-                    <button type="button" class="js-video-demo-path-upload input-group-text text-white panel-file-manager {{ (empty($webinar) or empty($webinar->video_demo_source) or $webinar->video_demo_source == 'upload') ? '' : 'd-none' }}" data-input="demo_video" data-preview="holder">
+                    <button type="button"
+                        class="js-video-demo-path-upload input-group-text text-white panel-file-manager {{ (empty($webinar) or empty($webinar->video_demo_source) or $webinar->video_demo_source == 'upload') ? '' : 'd-none' }}"
+                        data-input="demo_video" data-preview="holder">
                         <i data-feather="upload" width="18" height="18" class="text-white"></i>
                     </button>
 
-                    <button type="button" class="js-video-demo-path-links rounded-left input-group-text input-group-text-rounded-left text-white {{ (empty($webinar) or empty($webinar->video_demo_source) or $webinar->video_demo_source == 'upload') ? 'd-none' : '' }}">
+                    <button type="button"
+                        class="js-video-demo-path-links rounded-left input-group-text input-group-text-rounded-left text-white {{ (empty($webinar) or empty($webinar->video_demo_source) or $webinar->video_demo_source == 'upload') ? 'd-none' : '' }}">
                         <i data-feather="link" width="18" height="18" class="text-white"></i>
                     </button>
                 </div>
-                <input type="text" name="video_demo" id="demo_video" value="{{ !empty($webinar) ? $webinar->video_demo : old('video_demo') }}" class="form-control @error('video_demo')  is-invalid @enderror"/>
+                <input type="text" name="video_demo" id="demo_video"
+                    value="{{ !empty($webinar) ? $webinar->video_demo : old('video_demo') }}"
+                    class="form-control @error('video_demo')  is-invalid @enderror" />
                 @error('video_demo')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
         </div>
@@ -153,24 +179,29 @@
     <div class="col-12">
         <div class="form-group">
             <label class="input-label">{{ trans('public.description') }}</label>
-            <textarea id="summernote" name="description" class="form-control @error('description')  is-invalid @enderror" placeholder="{{ trans('forms.webinar_description_placeholder') }}">{!! (!empty($webinar) and !empty($webinar->translate($locale))) ? $webinar->translate($locale)->description : old('description')  !!}</textarea>
+            <textarea id="summernote" name="description" class="form-control @error('description')  is-invalid @enderror"
+                placeholder="{{ trans('forms.webinar_description_placeholder') }}">{!! (!empty($webinar) and !empty($webinar->translate($locale)))
+                    ? $webinar->translate($locale)->description
+                    : old('description') !!}</textarea>
             @error('description')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
             @enderror
         </div>
     </div>
 </div>
 
-@if($isOrganization)
+@if ($isOrganization)
     <div class="row">
         <div class="col-6">
 
             <div class="form-group mt-30 d-flex align-items-center">
-                <label class="cursor-pointer mb-0 input-label" for="privateSwitch">{{ trans('webinars.private') }}</label>
+                <label class="cursor-pointer mb-0 input-label"
+                    for="privateSwitch">{{ trans('webinars.private') }}</label>
                 <div class="ml-30 custom-control custom-switch">
-                    <input type="checkbox" name="private" class="custom-control-input" id="privateSwitch" {{ (!empty($webinar) and $webinar->private) ? 'checked' :  '' }}>
+                    <input type="checkbox" name="private" class="custom-control-input" id="privateSwitch"
+                        {{ (!empty($webinar) and $webinar->private) ? 'checked' : '' }}>
                     <label class="custom-control-label" for="privateSwitch"></label>
                 </div>
             </div>
@@ -183,14 +214,13 @@
 @push('scripts_bottom')
     <script src="/assets/vendors/summernote/summernote-bs4.min.js"></script>
 
-    @push('scripts_bottom')
-        <script>
-            var videoDemoPathPlaceHolderBySource = {
-                upload: '{{ trans('update.file_source_upload_placeholder') }}',
-                youtube: '{{ trans('update.file_source_youtube_placeholder') }}',
-                vimeo: '{{ trans('update.file_source_vimeo_placeholder') }}',
-                external_link: '{{ trans('update.file_source_external_link_placeholder') }}',
-            }
-        </script>
-    @endpush
+
+    <script>
+        var videoDemoPathPlaceHolderBySource = {
+            upload: '{{ trans('update.file_source_upload_placeholder') }}',
+            youtube: '{{ trans('update.file_source_youtube_placeholder') }}',
+            vimeo: '{{ trans('update.file_source_vimeo_placeholder') }}',
+            external_link: '{{ trans('update.file_source_external_link_placeholder') }}',
+        }
+    </script>
 @endpush
