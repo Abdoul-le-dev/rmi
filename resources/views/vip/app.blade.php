59d3f4f40c0d17c5884872e3813426e7d17e2238
Abdoul-le-dev <abdoulledev@gmail.com>
2025-12-20 20:37:30 +0100

    suite

--- resources/views/vip/app.blade.php
+++ resources/views/vip/app.blade.php
@@ -77,7 +77,7 @@
                             <img 
                                 src="https://i.pravatar.cc/150?img=12" 
                                 alt="Avatar" 
-                                class="w-6 h-6 rounded-full object-cover ring-2 ring-indigo-500"
+                                class="w-8 h-8 rounded-full object-cover ring-2 ring-indigo-500"
                             >
                         </button>
                         
