949cc4eb1aecaa334cbdf44aa19267e10cdb6364
Abdoul-le-dev <abdoulledev@gmail.com>
2026-02-10 18:09:14 +0100

    suite

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-//Auth::loginUsingId(22422);
+Auth::loginUsingId(22422);
 
 class PostController extends Controller
 {
