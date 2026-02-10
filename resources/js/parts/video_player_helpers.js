4ed59485c1492b6753c17e944007e76ea7d191de
Stanislas <stanislasbayord79@gmail.com>
2026-02-10 08:35:19 +0100

    feat: HLS QUALITY

--- resources/js/parts/video_player_helpers.js
+++ resources/js/parts/video_player_helpers.js
@@ -152,8 +152,17 @@
             playerInstance = videojs(playerId, playerData.options);
             playerInstance.ready(function () {
                 if (videoUrl.endsWith(".m3u8")) {
-                    // Initialisation du nouveau plugin compatible
+                    // 1. Activer le sélecteur dès que possible
                     this.qualitySelectorHls();
+
+                    // 2. Gestionnaire de secours pour s'assurer que le clic force le changement
+                    const qualityLevels = this.qualityLevels();
+
+                    // On force le player à accepter le changement immédiat
+                    qualityLevels.on("addqualitylevel", function (event) {
+                        let qualityLevel = event.qualityLevel;
+                        qualityLevel.enabled = true; // S'assure que le niveau est sélectionnable
+                    });
                 }
             });
 
