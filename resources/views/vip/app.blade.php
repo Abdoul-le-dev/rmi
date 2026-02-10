c175c3194960fed94d622fd65476e025a14a4fa8
Abdoul-le-dev <abdoulledev@gmail.com>
2026-02-10 11:30:48 +0100

    suite

--- resources/views/vip/app.blade.php
+++ resources/views/vip/app.blade.php
@@ -25,8 +25,8 @@
     <!-- ========================================
          LAYOUT PRINCIPAL (3 COLONNES)
     ======================================== -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-        <div class="grid grid-cols-1 lg:grid-cols-12 gap-6">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 ">
+        <div class="grid grid-cols-1 lg:grid-cols-9  xl:grid-cols-12 gap-6 ">
 
             <!-- ========================================
                  SIDEBAR GAUCHE - NAVIGATION
